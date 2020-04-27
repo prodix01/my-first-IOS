ffdd3ff084a0b7522911a11cd6ced597422a0ed3
kim <s158614@nishitech.ac.jp>
2020-04-27 12:37:51 +0900

    Initial Commit

---
+++ myfirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myfirstApp
+//
+//  Created by 김민규 on 2020/04/27.
+//  Copyright © 2020 kimmingyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
